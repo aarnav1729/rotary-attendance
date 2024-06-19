977f49ccb94d256ebfea8c1cacfca7182636a73f
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-06-19 16:14:47 +0530

    updated switch import from react-router-dom

--- rotary-attendance-frontend/src/index.js
+++ rotary-attendance-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
 import App from './App';
 import AttendanceRecords from './AttendanceRecords';
 import './index.css';
@@ -8,10 +8,10 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/records" component={AttendanceRecords} />
-      </Switch>
+      <Routes> {/* Updated from Switch to Routes */}
+        <Route path="/" element={<App />} /> {/* Updated Route */}
+        <Route path="/records" element={<AttendanceRecords />} /> {/* Updated Route */}
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
